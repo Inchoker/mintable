20022ac10e2ae3027295a53dedcf79864eabf60e
Jack Tri Le <Jack>
2023-09-10 18:47:51 +0800

    fix document client

--- src/create-nft.ts
+++ src/create-nft.ts
@@ -3,7 +3,7 @@ import * as AWS from 'aws-sdk';
 import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 
 // AWS.config.update({ region: 'us-east-1' });
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const dynamoDB = new DocumentClient();
 type NFTType = {
     name:string;
     desc:string;
